0df8f3b19e07dc4c34a22ce6ea4699d624653243
Oliver Luschnitz <oliver.luschnitz@gmail.com>
2025-06-12 06:42:39 +0200

    added cs file

---
+++ sln/Hawkesbury.Core/Core/ComponentModel/INotifyPropertyChangedExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hawkesbury.Core.ComponentModel
+{
+    public delegate bool EqualsMethod<T>(T x, T y);
+
+    public static class INotifyPropertyChangedExtensions
+    {
+        #region Definitions
+
+
+
+        #endregion
+
+        #region Extensions Methods
+
+        #endregion
+    }
+}
